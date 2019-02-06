d827d2c9e84401e08d03ed62752b0e3122f90afe
Viorel Contu <Viorel.Contu@endava.com>
2019-02-06 14:54:45 +0200

    Project setup: Spring Boot, Maven. Logback config

---
+++ src/main/java/com/endava/practice/roadmap/RoadmapApplication.java
@@ -0,0 +1,14 @@
+package com.endava.practice.roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmapApplication.class, args);
+	}
+
+}
+
